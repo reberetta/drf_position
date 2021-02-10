de08727bb1e616882940aea45cfe0e0848206a1b
Victor Hausen <hausen.victor@gmail.com>
2021-02-10 15:30:10 -0300

    Revert "views and routs implementation"

--- position/views.py
+++ position/views.py
@@ -1,12 +1,3 @@
 from django.shortcuts import render
 
-from position.models import Position
-
-from rest_framework import viewsets
-from rest_framework import permissions
-from position.serializers import PositionSerializer
-
-class PositionViewSet(viewsets.ModelViewSet):
-    queryset = Position.objects.all()
-    serialiser_class = PositionSerializer
-    permission_classes = [permissions.IsAuthenticated]
+# Create your views here.

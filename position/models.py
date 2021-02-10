5cc38463cbd0a667106061795284b1abc87a1e35
Regina Beretta <reginabmre@gmail.com>
2021-02-10 15:06:53 -0400

    fixing pep8

--- position/models.py
+++ position/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
+
 class Position(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=300)
     wage = models.DecimalField(decimal_places=2, max_digits=10)
     bonus = models.DecimalField(decimal_places=2, max_digits=10)
     bond_type = models.CharField(max_length=50)
-
-

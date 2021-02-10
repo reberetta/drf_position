2c978296e10f33a0a8695ab9713b273bca278fdc
Regina Beretta <reginabmre@gmail.com>
2021-02-10 14:25:19 -0400

    adding model and serializers

--- position/models.py
+++ position/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Position(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    wage = models.DecimalField(decimal_places=2, max_digits=10)
+    bonus = models.DecimalField(decimal_places=2, max_digits=10)
+    bond_type = models.CharField(max_length=50)
+
+
